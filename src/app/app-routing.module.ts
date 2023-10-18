08121137de950044484410a9f1948ba906cef79e
SimoNapo02 <simonematteonapolitano@gmail.com>
2023-10-18 14:50:54 +0200

    fix-portfolio.component <a>

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from './home/home.component'
 import { AboutComponent } from './about/about.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
-import { ProjectsComponent } from './projects/projects.component';
 import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'portfolio', component: PortfolioComponent }, 
-  { path: 'projects', component: ProjectsComponent },   
   { path: 'contact', component: ContactComponent },
 ];
 
